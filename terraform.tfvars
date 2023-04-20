6b00fecc8273f2ecea6e3dfb56bb8b12555766bb
Suvarna <rajesh.suvarna@capgemini.com>
2023-04-20 21:48:59 +0530

    first commit

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+bucket_name = "terraform-action-test-bucket-1"
+location = "us-central1"
+project_id = "terraform-actions"
+region = "us-central1"
+google_credentials = var.google_credentials
---
+++ variables.tf
@@ -0,0 +1,18 @@
+variable "bucket_name" {
+	type = string
+}
+
+variable "location" {
+	type = string
+}
+
+variable "project_id"{
+	type = string
+}
+
+variable "region" {
+	type = string
+}
+variable "google_credentials" {
+  type = string
+}
